99c067bb704199748d13f615fc56b1fa6d1f7f5d
TabanschiAlex <55706654+TabanschiAlex@users.noreply.github.com>
2020-04-13 02:05:13 +0300

    Add files via upload

--- Proiect NR4.cpp
+++ Proiect NR4.cpp
@@ -27,8 +27,8 @@ int main() {
 	do {
 		
 		cout << "\t\t Обработка данных в матрице \n\n" <<endl;
-	    cout << "1.De determinat ?ntr-o matrice valorile minim? ?i maxim? ?i pozi?iile lor." << endl;
-		cout << "2.De interschimbat 2 linii (variantele impare) sau 2 coloane (variantele pare)." << endl;
+	    cout << "1.Найти наибольшее и наименьшее значения в матрице, и их позиции." << endl;
+		cout << "2.Поменять местами строки или колонки." << endl;
 		cout << "3.De ad?ugat o linie ?i / sau coloan?, de completat cu caracteristica conform variantei." << endl;
 		cout << "4.De aranjat o matrice dup? linia sau coloana ad?ugat?." << endl;
 		cout << "5.De generat o matrice conform variantei.\n" << endl;
@@ -36,24 +36,25 @@ int main() {
 	    cout << "\t|6 5 4|" <<endl;
 	    cout << "\t|7 8 9|\n" <<endl;
 	    cout << "0.Выход" <<endl;
-	    cout << "\t\tВыберите функцию ==> ";
-	    cin  >> key1;
+	    key1 = getch();
 	    
 	    switch (key1) {
+	    	
 	    	case '1':
+	    		
 	    		do {
 	    			
 	    			system("CLS");
-	    			cout << "\t\t De interschimbat 2 linii in matrice \n\n";
+	    			cout << "\t\t Найти наибольшее и наименьшее значения в матрице, и их позиции \n\n";
             		cout << "1. Ввести матрицу вручную" << endl;
-                	cout << "2. Matricea generata aleator" << endl;
+                	cout << "2. Сгенерировать матрицу" << endl;
                 	cout << "0. Главное меню\n\n";
-                	cout << "\t\tВыберите функцию ==> ";
-					cin >> key2;
+					key2 = getch();
 					
 					switch (key2) {
 						
 						case '1':
+							
 							system("CLS");
 							cout << "Введите количетсво строк = ";
 							cin >> row;
@@ -61,11 +62,16 @@ int main() {
 							cin >> column;
 							
 							manualMatrix( matrix, row, column );
+							searchMax( matrix, row, column );
+							searchMin( matrix, row ,column );
 							
+							cout << endl;
 							cout << "\t( 'Enter' - вернуться в меню )" << endl;
 							getch();
 							break;
+							
 						case '2':
+							
 							system("CLS");
 							cout << "Введите количетсво строк = ";
 							cin >> row;
@@ -73,17 +79,67 @@ int main() {
 							cin >> column;
 							
 							randomMatrix( matrix, row, column );
+							searchMax( matrix, row, column );
+							searchMin( matrix, row ,column );
 							
+							cout << endl;
 							cout << "\t( 'Enter' - вернуться в меню )" << endl;
 							getch();
 							break;
 					}
 					
 				} while ( key2 != 0 );
-	    		getch();
         		break;
+        		
         	case '2':
-	    		system("CLS");
+        		
+	    		do {
+	    			
+	    			system("CLS");
+	    			cout << "\t\t Найти наибольшее и наименьшее значения в матрице, и их позиции \n\n";
+            		cout << "1. Ввести матрицу вручную" << endl;
+                	cout << "2. Сгенерировать матрицу" << endl;
+                	cout << "0. Главное меню\n\n";
+					key2 = getch();
+					
+					switch (key2) {
+						
+						case '1':
+							
+							system("CLS");
+							cout << "Введите количетсво строк = ";
+							cin >> row;
+							cout << "Введите количество колонок = ";
+							cin >> column;
+							
+							manualMatrix( matrix, row, column );
+							searchMax( matrix, row, column );
+							searchMin( matrix, row ,column );
+							
+							cout << endl;
+							cout << "\t( 'Enter' - вернуться в меню )" << endl;
+							getch();
+							break;
+							
+						case '2':
+							
+							system("CLS");
+							cout << "Введите количетсво строк = ";
+							cin >> row;
+							cout << "Введите количество колонок = ";
+							cin >> column;
+							
+							randomMatrix( matrix, row, column );
+							searchMax( matrix, row, column );
+							searchMin( matrix, row ,column );
+							
+							cout << endl;
+							cout << "\t( 'Enter' - вернуться в меню )" << endl;
+							getch();
+							break;
+					}
+					
+				} while ( key2 != 0 );
 	    		getch();
         		break;
         	case '3':
@@ -163,6 +219,7 @@ void searchMax ( int matrix[limit][limit], int row, int column ) {
 	}
 	cout << "Наибольшее значение в матрице = " << max << " в позиции ";
 	Position ( matrix, row, column, max );
+	cout << endl;
 }
 
 void searchMin ( int matrix[limit][limit], int row, int column ) {
@@ -177,4 +234,9 @@ void searchMin ( int matrix[limit][limit], int row, int column ) {
 	}
 	cout << "Наименьшее значение в матрице = " << min << " в позиции ";
 	Position ( matrix, row, column, min );
+	cout << endl;
+}
+
+void changeRows ( matrix[limit][limit], row, column ) {
+	
 }
